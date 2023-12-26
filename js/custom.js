b88511be9cafc99bc543c126c7141e0abbe82a29
rishikapadia7 <rishikap7@gmail.com>
2023-12-26 18:17:09 -0500

    Added a few topics for word connections, and have random generation, but with some repeats.

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,32 @@
+//list of game name constants
+GAME_WORDCONNECTIONS_INDEX = 0;
+GAME_NAME3_INDEX = 1;
+GAME_OVERKILL_INDEX = 2;
+GAME_SAYWITHOUTSAYING_INDEX = 3;
+GAME_NOENGLISH_INDEX = 4;
+
+//word connection strings array
+TOPICS_WORDCONNECTIONS = [
+    "Mother-In-Law",
+    "Monkeys",
+    "Dog",
+    "Spicy",
+    "Pepper",
+    "Mouse",
+    "Grass",
+    "Books",
+    "Computer",
+    "Carrot"
+];
+
+
 $(document).ready(function(){
    $("#word-connection").click(function(){
     $(this).closest(".middle-div").find("#w-connection").addClass("active")
    })
    $(".btn-close").click(function(){
     $(this).closest(".middle-div").find("#w-connection").removeClass("active")
+
    })
    $("#no-english").click(function(){
     $(this).closest(".middle-div").find("#noenglish").addClass("active")
@@ -11,5 +34,37 @@ $(document).ready(function(){
    $(".btn-close").click(function(){
     $(this).closest(".middle-div").find("#noenglish").removeClass("active")
    })
+
+   //Word connections start button press
+   $(".start-button").click(function(){
+    $(this).parent().fadeOut(400);
+    //Get word from dicitionary and replace it's text
+    SetNextTopic(GAME_WORDCONNECTIONS_INDEX, "#WordConnectionsTextID");
+    $("#startedWordConnectionsPanel").fadeIn(400);
+   })
+
+   $("#NextWordConnectionButton").click(function() {
+    $(this).parent().hide();
+
+    $(this).parent().fadeIn(500);
+    
+    SetNextTopic(GAME_WORDCONNECTIONS_INDEX, "#WordConnectionsTextID");
+   })
    
 })
+
+function getRandomInt(totalItems){
+    return Math.floor(Math.random() * totalItems);
+}
+
+function SetNextTopic(gameIndex, textObjectID){
+    if(gameIndex == GAME_WORDCONNECTIONS_INDEX){
+        var maxlength = TOPICS_WORDCONNECTIONS.length;
+        var randomInt = getRandomInt(maxlength);
+        $(textObjectID).text(TOPICS_WORDCONNECTIONS[randomInt % maxlength]);
+    } else {
+        $(textObjectID).text("testing2");
+    }
+
+    
+}
